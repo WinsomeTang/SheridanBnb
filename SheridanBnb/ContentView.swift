e00b311544260e464247071f1a32345542c7ffdf
Winsome Tang <tangwins@sheridancollege.ca>
2024-01-04 13:55:28 -0500

    Initial Commit

---
+++ SheridanBnb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SheridanBnb
+//
+//  Created by Winsome Tang on 2024-01-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SheridanBnb/SheridanBnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheridanBnbApp.swift
+//  SheridanBnb
+//
+//  Created by Winsome Tang on 2024-01-04.
+//
+
+import SwiftUI
+
+@main
+struct SheridanBnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
